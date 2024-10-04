41cbff98ad3536d4ca39d6723ae2c201af31ae62
rojojun <zoloman316@gmail.com>
2024-10-05 00:43:30 +0900

    [settings] project setting for first time

---
+++ src/main/java/com/rojojun/studycafe/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package com.rojojun.studycafe;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/com/rojojun/studycafe/exception/AppException.java
@@ -0,0 +1,9 @@
+package com.rojojun.studycafe.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
